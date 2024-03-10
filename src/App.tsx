cca7d089ab934ee6568b5066dfc41f54044534c3
laihaoqiang <laihaoqiang34@gmail.com>
2024-03-11 03:05:49 +1000

    configuration of development environment

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+  const carouselSlidesData = [
+    {
+      content:
+        "Tomorrow, you will be released. If you are bored of brawling with thieves and want to achieve something there is a rare blue flower that grows on the eastern slopes. Pick one of these flowers. If you can carry it to the top of the mountain, you may find what you were looking for in the first place.",
+      author: "Bane",
+      source: "facebook"
+    }, {
+      content:
+        "You have learn to bury your guilt with anger. I will teach you to confront it and to face the truth.",
+      author: "Ra's Al Ghul",
+      source: "Snapchat"
+    }, {
+      content:
+        "Introduce a little anarchy, upset the established order and everything becomes chaos. I'm an agent of chaos. Oh, and you know the thing about chaos? It's fair.",
+      author: "Joker",
+      source: "facebook"
+    }, {
+      content:
+        "I can't do that as Bruce Wayne... as a man. I'm flesh and blood. I can be ignored, destroyed. But as a symbol, I can be incorruptible, I can be everlasting.",
+      author: "Bruce Wayne",
+      source: "facebook"
+    }, {
+      content:
+        "But it's not who you are underneath... it's what you do that defines you.",
+      author: "Rachel Dawes",
+      source: "twitter"
+    }, {
+      content:
+        "When their enemies were at the gates the Romans would suspend democracy and appoint one man to protect the city. It wasn't considered an honor, it was a public service.",
+      author: "John Blake",
+      source: "Google+"
+    }, {
+      content:
+        "Master Wayne, you've been gone a long time. You look very fashionable. Apart from the mud.",
+      author: "Alfred Pennyworth",
+      source: "twitter"
+    }
+  ];
 
+  const CarouselLeftArrow = () => {
+    return (
+      <a href="#"></a>
+    )
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
